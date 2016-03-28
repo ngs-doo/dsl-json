05431ebe4556a654cdf269d1c9daa5e509505e54
Rikard Pavelic <rikard@ngs.hr>
2016-03-29 00:51:39 +0200

    Minor performance improvement.
    Move MIN check with negative check.
    Avoid SHIFT array lookup.
    Lazy modulo operation.

--- library/src/main/java/com/dslplatform/json/NumberConverter.java
+++ library/src/main/java/com/dslplatform/json/NumberConverter.java
@@ -483,44 +483,43 @@ public abstract class NumberConverter {
 	}
 
 	private static int serialize(final byte[] buf, int pos, final int value) {
-		if (value == Integer.MIN_VALUE) {
-			for (int i = 0; i < MIN_INT.length; i++) {
-				buf[pos + i] = MIN_INT[i];
-			}
-			return pos + MIN_INT.length;
-		}
 		int i;
 		if (value < 0) {
+			if (value == Integer.MIN_VALUE) {
+				for (int x = 0; x < MIN_INT.length; x++) {
+					buf[pos + x] = MIN_INT[x];
+				}
+				return pos + MIN_INT.length;
+			}
 			i = -value;
 			buf[pos++] = MINUS;
 		} else {
 			i = value;
 		}
 		final int q1 = i / 1000;
-		final int r1 = i - q1 * 1000;
 		if (q1 == 0) {
-			pos += writeFirstBuf(buf, DIGITS[r1], pos);
+			pos += writeFirstBuf(buf, DIGITS[i], pos);
 			return pos;
 		}
+		final int r1 = i - q1 * 1000;
 		final int q2 = q1 / 1000;
-		final int r2 = q1 - q2 * 1000;
 		if (q2 == 0) {
 			final int v1 = DIGITS[r1];
-			final int v2 = DIGITS[r2];
+			final int v2 = DIGITS[q1];
 			int off = writeFirstBuf(buf, v2, pos);
 			writeBuf(buf, v1, pos + off);
 			return pos + 3 + off;
 		}
+		final int r2 = q1 - q2 * 1000;
 		final long q3 = q2 / 1000;
-		final int r3 = (int) (q2 - q3 * 1000);
 		final int v1 = DIGITS[r1];
 		final int v2 = DIGITS[r2];
-		final int v3 = DIGITS[r3];
 		if (q3 == 0) {
-			pos += writeFirstBuf(buf, v3, pos);
+			pos += writeFirstBuf(buf, DIGITS[q2], pos);
 		} else {
+			final int r3 = (int) (q2 - q3 * 1000);
 			buf[pos++] = (byte) (q3 + '0');
-			writeBuf(buf, v3, pos);
+			writeBuf(buf, DIGITS[r3], pos);
 			pos += 3;
 		}
 		writeBuf(buf, v2, pos);
@@ -633,12 +632,15 @@ public abstract class NumberConverter {
 		}
 	}
 
-	private final static int[] SHIFTS = {16, 8, 0};
-
 	private static int writeFirstBuf(final byte[] buf, final int v, int pos) {
 		final int start = v >> 24;
-		for (int x = start; x < 3; x++)
-			buf[pos++] = (byte) (v >> SHIFTS[x]);
+		if (start == 0) {
+			buf[pos++] = (byte) (v >> 16);
+			buf[pos++] = (byte) (v >> 8);
+		} else if (start == 1) {
+			buf[pos++] = (byte) (v >> 8);
+		}
+		buf[pos] = (byte) v;
 		return 3 - start;
 	}
 
@@ -658,66 +660,65 @@ public abstract class NumberConverter {
 	}
 
 	private static int serialize(final byte[] buf, int pos, final long value) {
-		if (value == Long.MIN_VALUE) {
-			for (int i = 0; i < MIN_LONG.length; i++) {
-				buf[pos + i] = MIN_LONG[i];
-			}
-			return pos + MIN_LONG.length;
-		}
 		long i;
 		if (value < 0) {
+			if (value == Long.MIN_VALUE) {
+				for (int x = 0; x < MIN_LONG.length; x++) {
+					buf[pos + x] = MIN_LONG[x];
+				}
+				return pos + MIN_LONG.length;
+			}
 			i = -value;
 			buf[pos++] = MINUS;
 		} else {
 			i = value;
 		}
 		final long q1 = i / 1000;
-		final int r1 = (int) (i - q1 * 1000);
 		if (q1 == 0) {
-			pos += writeFirstBuf(buf, DIGITS[r1], pos);
+			pos += writeFirstBuf(buf, DIGITS[(int)i], pos);
 			return pos;
 		}
+		final int r1 = (int) (i - q1 * 1000);
 		final long q2 = q1 / 1000;
-		final int r2 = (int) (q1 - q2 * 1000);
 		if (q2 == 0) {
 			final int v1 = DIGITS[r1];
-			final int v2 = DIGITS[r2];
+			final int v2 = DIGITS[(int)q1];
 			int off = writeFirstBuf(buf, v2, pos);
 			writeBuf(buf, v1, pos + off);
 			return pos + 3 + off;
 		}
+		final int r2 = (int) (q1 - q2 * 1000);
 		final long q3 = q2 / 1000;
-		final int r3 = (int) (q2 - q3 * 1000);
 		if (q3 == 0) {
 			final int v1 = DIGITS[r1];
 			final int v2 = DIGITS[r2];
-			final int v3 = DIGITS[r3];
+			final int v3 = DIGITS[(int)q2];
 			pos += writeFirstBuf(buf, v3, pos);
 			writeBuf(buf, v2, pos);
 			writeBuf(buf, v1, pos + 3);
 			return pos + 6;
 		}
+		final int r3 = (int) (q2 - q3 * 1000);
 		final int q4 = (int) (q3 / 1000);
-		final int r4 = (int) (q3 - q4 * 1000);
 		if (q4 == 0) {
 			final int v1 = DIGITS[r1];
 			final int v2 = DIGITS[r2];
 			final int v3 = DIGITS[r3];
-			final int v4 = DIGITS[r4];
+			final int v4 = DIGITS[(int)q3];
 			pos += writeFirstBuf(buf, v4, pos);
 			writeBuf(buf, v3, pos);
 			writeBuf(buf, v2, pos + 3);
 			writeBuf(buf, v1, pos + 6);
 			return pos + 9;
 		}
+		final int r4 = (int) (q3 - q4 * 1000);
 		final int q5 = q4 / 1000;
-		final int r5 = q4 - q5 * 1000;
 		if (q5 == 0) {
 			final int v1 = DIGITS[r1];
 			final int v2 = DIGITS[r2];
 			final int v3 = DIGITS[r3];
 			final int v4 = DIGITS[r4];
-			final int v5 = DIGITS[r5];
+			final int v5 = DIGITS[q4];
 			pos += writeFirstBuf(buf, v5, pos);
 			writeBuf(buf, v4, pos);
 			writeBuf(buf, v3, pos + 3);
@@ -725,19 +726,19 @@ public abstract class NumberConverter {
 			writeBuf(buf, v1, pos + 9);
 			return pos + 12;
 		}
+		final int r5 = q4 - q5 * 1000;
 		final int q6 = q5 / 1000;
-		final int r6 = q5 - q6 * 1000;
 		final int v1 = DIGITS[r1];
 		final int v2 = DIGITS[r2];
 		final int v3 = DIGITS[r3];
 		final int v4 = DIGITS[r4];
 		final int v5 = DIGITS[r5];
-		final int v6 = DIGITS[r6];
 		if (q6 == 0) {
-			pos += writeFirstBuf(buf, v6, pos);
+			pos += writeFirstBuf(buf, DIGITS[q5], pos);
 		} else {
+			final int r6 = q5 - q6 * 1000;
 			buf[pos++] = (byte) (q6 + '0');
-			writeBuf(buf, v6, pos);
+			writeBuf(buf, DIGITS[r6], pos);
 			pos += 3;
 		}
 		writeBuf(buf, v5, pos);
