0840300112cf5f6fe8c4cedf8f4b60710ab629d0
Rikard Pavelic <rikard@ngs.hr>
2019-03-30 17:32:18 +0100

    Test for sanity check for correct float primitives behavior
    IncludeToMinimal behaves as expected

--- tests-java8/src/test/java/com/dslplatform/json/PrimitivesTest.java
+++ tests-java8/src/test/java/com/dslplatform/json/PrimitivesTest.java
@@ -3,6 +3,7 @@ package com.dslplatform.json;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class PrimitivesTest {
@@ -43,5 +44,27 @@ public class PrimitivesTest {
 		} catch (IOException ex) {
 			Assert.assertTrue(ex.getMessage().contains("Error parsing number at position: 5"));
 		}
+	}
+
+	@CompiledJson
+	public static class FloatPrimitive {
+		@JsonAttribute(includeToMinimal = JsonAttribute.IncludePolicy.NON_DEFAULT)
+		public float f1;
+		@JsonAttribute(includeToMinimal = JsonAttribute.IncludePolicy.ALWAYS)
+		public float f2;
+	}
+
+	@Test
+	public void noDefaultValueForFloat() throws IOException {
+		FloatPrimitive fp = new FloatPrimitive();
+		fp.f1 = 0;
+		fp.f2 = 0;
+		DslJson<Object> minimal = new DslJson<>(new DslJson.Settings<>().skipDefaultValues(true).includeServiceLoader());
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		minimal.serialize(fp, os);
+		Assert.assertEquals("{\"f2\":0.0}", os.toString("UTF-8"));
+		FloatPrimitive res = minimal.deserialize(FloatPrimitive.class, os.toByteArray(), os.size());
+		Assert.assertEquals(fp.f1, res.f1, 0);
+		Assert.assertEquals(fp.f2, res.f2, 0);
 	}
 }
