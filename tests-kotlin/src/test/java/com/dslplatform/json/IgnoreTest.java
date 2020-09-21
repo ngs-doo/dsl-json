b0634fe0e101ad235549f04c8684745050cafeec
abnud1 <ahmad13932013@hotmail.com>
2020-09-21 11:33:56 +0300

    Fix: kotlin tests are now running

--- tests-kotlin/src/test/java/com/dslplatform/json/IgnoreTest.java
+++ tests-kotlin/src/test/java/com/dslplatform/json/IgnoreTest.java
@@ -2,22 +2,25 @@ package com.dslplatform.json;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class IgnoreTest {
-
-	private final DslJson<Object> dslJson = new DslJson<>();
-
-	@Test
-	public void ignoresAreNotInOutput() throws IOException {
-		HasIgnoreWithDefault model = new HasIgnoreWithDefault();
-		model.stringFieldWithIgnoreFlag = "this string is ignored";
-		model.stringWhichShouldBeInPlace = "this string should be in json";
-		OutputStream os = new ByteArrayOutputStream();
-		dslJson.serialize(model, os);
-		Assert.assertEquals("{\"someString\":\"some-string\",\"stringWhichShouldBeInPlace\":\"this string should be in json\"}", os.toString());
-	}
+    private DslJson dslJson = new DslJson();
+    public IgnoreTest(){}
+    @Test
+    public void ignoresAreNotInOutput() throws IOException, InterruptedException {
+        HasIgnoreWithDefault model = new HasIgnoreWithDefault();
+        model.stringFieldWithIgnoreFlag = "this string is ignored";
+        model.stringWhichShouldBeInPlace = "this string should be in json";
+        OutputStream os = new ByteArrayOutputStream();
+        dslJson.serialize(model, os);
+        Assert.assertEquals(
+                "{\"someString\":\"some-string\",\"stringWhichShouldBeInPlace\":\"this string should be in json\"}",
+                os.toString()
+        );
+        Thread.sleep(5000);
+        System.out.println("Hello from test");
+    }
 }
